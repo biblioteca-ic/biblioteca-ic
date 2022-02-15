48864ad112029376fc9219a0da9eaa450072e41a
gideaopinheiro <gpss@ic.ufal.br>
2022-02-15 20:08:06 -0300

    feat: altera password validation

--- back-end/src/presentation/validation/validators/password-validator.ts
+++ back-end/src/presentation/validation/validators/password-validator.ts
@@ -5,7 +5,7 @@ export class PasswordValidator implements Validation {
   constructor (private readonly fieldName: string) { }
 
   validate (input: any): Error {
-    if (!input[this.fieldName].match(/^(?=.{6,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#)!&($%^&+=]).*$/)) {
+    if (!input[this.fieldName].match(/^(?=.{6,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#)!&*($%^&+=]).*$/)) {
       return new InvalidFormatError('password')
     }
   }
