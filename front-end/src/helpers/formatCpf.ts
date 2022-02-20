9a1f97378b4b28afa5fdd6a1e09289424f62b280
Karen Gomes <kngs@ic.ufal.br>
2022-02-20 00:57:12 -0300

    fix: 🐛 change format cpf

--- front-end/src/helpers/formatCpf.ts
+++ front-end/src/helpers/formatCpf.ts
@@ -2,5 +2,5 @@ export const formatCpf = (cpf: string): string => {
   if (cpf.length !== 11) {
     return 'CPF inválido';
   }
-  return `${cpf.substring(0, 3)}-${cpf.substring(3, 6)}-${cpf.substring(6, 9)}.${cpf.substring(9, 11)}`;
+  return `${cpf.substring(0, 3)}.${cpf.substring(3, 6)}.${cpf.substring(6, 9)}-${cpf.substring(9, 11)}`;
 };
