5d6eb656f4056ae3581c667c74443b8073a25b64
gideaopinheiro <gpss@ic.ufal.br>
2022-02-19 23:27:36 -0300

    Revert "Add identityAuth to route"
    This reverts commit ebf754a01fa187a5fec00e687d1d165665f84b16.

--- back-end/src/main/routes/create-user-route.ts
+++ back-end/src/main/routes/create-user-route.ts
@@ -3,5 +3,6 @@ import { identityAuth } from '../../infra/middlewares/identity-auth'
 import { makeCreateUserController } from '../factories/make-create-user-controller'
 
 export default (router: Router): void => {
-  router.post('/users', identityAuth, async (req, res) => makeCreateUserController(req, res))
+  //router.post('/users', identityAuth, async (req, res) => makeCreateUserController(req, res))
+  router.post('/users', async (req, res) => makeCreateUserController(req, res))
 }
