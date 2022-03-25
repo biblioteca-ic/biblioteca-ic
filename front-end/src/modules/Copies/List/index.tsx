671190425fc18aa0f370efaa59eaec813bf96f85
Karen Gomes <kngs@ic.ufal.br>
2022-03-25 14:39:49 -0300

    add logic to add new copy

--- front-end/src/modules/Copies/List/index.tsx
+++ front-end/src/modules/Copies/List/index.tsx
@@ -1,25 +1,28 @@
-import * as React from 'react';
+import React, { useState, useRef } from 'react';
 import {
   Table,
   Thead,
   Tr,
   Th,
-  Td,
   Tbody,
   Button,
   Box,
   Heading,
-  Link,
   InputGroup,
   Input,
   InputRightElement,
   Text,
-  IconButton,
+  useToast,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
 } from '@chakra-ui/react';
-import { useHistory } from 'react-router-dom';
-import { BsInfoCircle, BsSearch, BsTrashFill } from 'react-icons/bs';
-import { FaUserPlus } from 'react-icons/fa';
+import { BsSearch } from 'react-icons/bs';
 import { AddIcon } from '@chakra-ui/icons';
+import { AxiosError } from 'axios';
 import { useAuth } from '../../../providers/AuthProvider';
 // import { api } from '../../../services/api';
 import { copiesMock } from '../../../services/mocks';
@@ -31,11 +34,19 @@ interface CopyTypeSearch extends CopyBookType {
 }
 
 const CopiesList = ({ book }: { book: BookType }) => {
-  const history = useHistory();
   const [copies, setCopies] = React.useState<CopyTypeSearch[]>([]);
   const [copiesSearch, setCopiesSearch] = React.useState<CopyTypeSearch[]>([]);
 
+  const toast = useToast();
   const { user } = useAuth();
+
+  const [isOpenAddNewCopy, setIsOpenAddNewCopy] = useState(false);
+  const onCloseAddNewCopy = () => setIsOpenAddNewCopy(false);
+  const cancelRefAddNewCopy = useRef<HTMLButtonElement>(null);
+
+  const clickAddNewCopy = () => {
+    setIsOpenAddNewCopy(true);
+  };
 
   const getAllCopies = async () => {
     try {
@@ -73,60 +84,102 @@ const CopiesList = ({ book }: { book: BookType }) => {
     }
   };
 
-  return (
-    <Box p={8}>
-      <Box display="flex" mb={10} flexDirection="column" justifyContent="space-between" margin="auto">
-        <Box display="flex" w="100%" mb={4} alignItems="center" justifyContent={user ? 'space-between' : 'left'}>
-          <Heading color="teal" textAlign="center" mr={2}>
-            {`Cópias do livro ${book.title}`}
-          </Heading>
-          {user && (
-            <Button
-              leftIcon={<AddIcon />}
-              // onClick={() => history.push('register')}
-              colorScheme="teal"
-              variant="outline"
-            >
-              Criar nova
-            </Button>
-          )}
-        </Box>
+  const addNewCopy = async () => {
+    try {
+      // await api.post();
+      toast({
+        title: 'Nova cópia criada com sucesso',
+        status: 'success',
+        position: 'top-right',
+        isClosable: true,
+      });
 
-        <Box minW="20%" w="25%" mb={user ? 6 : 0}>
-          <InputGroup color="teal">
-            <Input placeholder="Buscar" bg="white" onChange={handleChange} />
-            <InputRightElement>
-              <BsSearch />
-            </InputRightElement>
-          </InputGroup>
+      onCloseAddNewCopy();
+
+      setTimeout(() => {
+        window.location.reload();
+      }, 1400);
+    } catch (error) {
+      const err = error as AxiosError;
+      toast({
+        title: 'Ocorreu um erro ao criar uma nova cópia',
+        description: err?.message ? err?.message : 'Tente novamente mais tarde',
+        status: 'error',
+        position: 'top-right',
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <>
+      <Box p={8}>
+        <Box display="flex" mb={10} flexDirection="column" justifyContent="space-between" margin="auto">
+          <Box display="flex" w="100%" mb={4} alignItems="center" justifyContent={user ? 'space-between' : 'left'}>
+            <Heading color="teal" textAlign="center" mr={2}>
+              {`Cópias do livro ${book.title}`}
+            </Heading>
+            {user && (
+              <Button leftIcon={<AddIcon />} onClick={clickAddNewCopy} colorScheme="teal" variant="outline">
+                Criar nova
+              </Button>
+            )}
+          </Box>
+
+          <Box minW="20%" w="25%" mb={user ? 6 : 0}>
+            <InputGroup color="teal">
+              <Input placeholder="Buscar" bg="white" onChange={handleChange} />
+              <InputRightElement>
+                <BsSearch />
+              </InputRightElement>
+            </InputGroup>
+          </Box>
         </Box>
+        {copies.length !== 0 ? (
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Identificador</Th>
+                <Th>Status</Th>
+                <Th>Ações</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {copiesSearch.map((copyFilter: CopyBookType) => {
+                return <CopiesBookItem key={copyFilter.id} copyBook={copyFilter} />;
+              })}
+            </Tbody>
+          </Table>
+        ) : (
+          <>
+            <Text textAlign="center">Não há cópias cadastradas.</Text>
+          </>
+        )}
       </Box>
-      {copies.length !== 0 ? (
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              <Th>Identificador</Th>
-              <Th>Status</Th>
-              <Th>Ações</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {copiesSearch.map((copyFilter: CopyBookType) => {
-              return <CopiesBookItem key={copyFilter.id} copyBook={copyFilter} />;
-            })}
-          </Tbody>
-        </Table>
-      ) : (
-        <>
-          <Text>Não há cópias cadastradas</Text>
-          <Link href="/register" textDecoration="none">
-            <Button colorScheme="teal" leftIcon={<FaUserPlus />}>
-              Criar cópia
-            </Button>
-          </Link>
-        </>
-      )}
-    </Box>
+
+      <AlertDialog isOpen={isOpenAddNewCopy} leastDestructiveRef={cancelRefAddNewCopy} onClose={onCloseAddNewCopy}>
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Criar nova cópia
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Tem certeza que deseja criar uma nova cópia do livr <strong>&quot;{book.title}&quot;</strong>?
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button colorScheme="green" onClick={addNewCopy}>
+                Sim
+              </Button>
+              <Button ref={cancelRefAddNewCopy} onClick={onCloseAddNewCopy} ml={3}>
+                Não
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 };
 
