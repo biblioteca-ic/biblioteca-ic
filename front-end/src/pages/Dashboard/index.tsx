e2a25ee6b7f09cb3214041f3e0346d5485ae8093
Karen Gomes <kngs@ic.ufal.br>
2022-03-29 12:39:03 -0300

    add links in dashboard

--- front-end/src/pages/Dashboard/index.tsx
+++ front-end/src/pages/Dashboard/index.tsx
@@ -1,12 +1,52 @@
 import * as React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Icon, Heading, Link } from '@chakra-ui/react';
+import { ImBooks, ImProfile, ImUsers } from 'react-icons/im';
+// import { FaUserFriends } from 'react-icons/fa';
 import { Page } from '../../components/Page';
+import { useAuth } from '../../providers/AuthProvider';
 
 const Dashboard = () => {
+  const { user } = useAuth();
   return (
     <Page>
       <Box textAlign="center" fontSize="xl" p={8}>
-        <Text>Hello, world</Text>
+        <Box w="70%" margin="auto">
+          <Heading color="teal" textAlign="center" mb={6}>
+            Funcionalidades:
+          </Heading>
+          <Link color="teal" href="/books" _focus={{ boxShadow: 'none' }}>
+            <Box border="1px" p={4} display="flex" alignItems="center" justifyContent="center" mb={2}>
+              <Icon as={ImBooks} fontSize="2rem" mr={2} />
+              <Heading fontSize="md" textTransform="uppercase">
+                Livros
+              </Heading>
+            </Box>
+          </Link>
+          {user && (
+            <>
+              <Link color="teal" href="/profile" _focus={{ boxShadow: 'none' }}>
+                <Box border="1px" p={4} display="flex" alignItems="center" justifyContent="center" mb={2}>
+                  <Icon as={ImProfile} fontSize="2rem" mr={2} />
+                  <Heading fontSize="md" textTransform="uppercase">
+                    Meu Perfil
+                  </Heading>
+                </Box>
+              </Link>
+              {user.admin && (
+                <>
+                  <Link color="teal" href="/users" _focus={{ boxShadow: 'none' }}>
+                    <Box border="1px" p={4} display="flex" alignItems="center" justifyContent="center" mb={2}>
+                      <Icon as={ImUsers} fontSize="2rem" mr={2} />
+                      <Heading fontSize="md" textTransform="uppercase">
+                        Usuários
+                      </Heading>
+                    </Box>
+                  </Link>
+                </>
+              )}
+            </>
+          )}
+        </Box>
       </Box>
     </Page>
   );
