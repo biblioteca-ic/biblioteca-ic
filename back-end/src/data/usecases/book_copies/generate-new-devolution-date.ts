10cc42c337de7508e92dfa62e8668cba8816a020
Mateus Pereira <bmp.mateus@gmail.com>
2022-04-25 00:25:07 -0300

    feat: New devolution date generator implemented

--- back-end/src/data/usecases/book_copies/generate-new-devolution-date.ts
+++ back-end/src/data/usecases/book_copies/generate-new-devolution-date.ts
@@ -1,8 +1,12 @@
+import { devolution_limit, new_devolution } from "../../../domain/contracts/borrowing"
+
 export class GenerateNewDevolutionDate {
   async generate(current: Date = new Date(), lease: Date = new Date()): Promise<Date> {
-    // TODO: Calculate new devolution date picking new_devolution value and adding to current date
-    // TODO: Calculate devolution limit date adding devolution_limit value to lease date
-    // TODO: If new devolution date is higher than devolution limit, return devolution limit
-    return new Date() // TODO: return new devolution date
+    const devolution = new Date(current.getTime())
+    devolution.setDate(devolution.getDate() + new_devolution)
+    const limit = new Date(lease.getTime())
+    limit.setDate(limit.getDate() + devolution_limit)
+    if ( devolution > limit ) return limit
+    return devolution
   }
 }
